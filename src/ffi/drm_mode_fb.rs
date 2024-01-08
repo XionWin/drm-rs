89882dbe3d736c7f5dbe3a5aee46a3faa1d74118
XionWin <xionwin@hotmail.com>
2024-01-09 04:57:43 +0800

    Update

--- src/ffi/drm_mode_fb.rs
+++ src/ffi/drm_mode_fb.rs
@@ -1,7 +1,6 @@
 #[repr(C)]
 #[derive(Debug)]
-pub struct DrmModeFramebuffer
-{
+pub struct DrmModeFramebuffer {
     pub fb_id: libc::c_uint,
     pub width: libc::c_uint,
     pub height: libc::c_uint,
@@ -15,12 +14,19 @@ pub struct DrmModeFramebuffer
 #[link(name = "drm")]
 extern "C" {
     pub fn drmModeGetFB(fd: libc::c_int, fb_id: libc::c_uint) -> *const DrmModeFramebuffer;
-    #[allow(dead_code)]
-    pub fn drmModeAddFB2(fd: libc::c_int, width: libc::c_uint, height: libc::c_uint,
-        pixel_format: libc::c_uint, bo_handles: *const *const libc::c_void,
-        pitches: *const libc::c_uint, offsets: *const libc::c_uint,
-        buf_id: *mut libc::c_uint, flags: libc::c_uint) -> libc::c_int;
-    
-
     pub fn drmModeFreeFB(ptr: *const DrmModeFramebuffer);
+    // #[allow(dead_code)]
+    // pub fn drmModeAddFB2(
+    //     fd: libc::c_int,
+    //     width: libc::c_uint,
+    //     height: libc::c_uint,
+    //     pixel_format: libc::c_uint,
+    //     bo_handles: *const *const libc::c_void,
+    //     pitches: *const libc::c_uint,
+    //     offsets: *const libc::c_uint,
+    //     buf_id: *mut libc::c_uint,
+    //     flags: libc::c_uint,
+    // ) -> libc::c_int;
+
+    
 }
